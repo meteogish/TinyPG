1af2f8b4a33fae1856c68774fcfbba708e95623f
meteogish <meteoprogish@gmail.com>
2022-12-13 13:06:47 +0100

    misc

---
+++ tests/TinyPG.UnitTests/RoslynParserCodeGeneratorTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TinyPG.CodeGenerators.Roslyn;
+using TinyPG.Compiler;
+
+namespace TinyPG.UnitTests
+{
+    [TestClass]
+    public class RoslynParserCodeGeneratorTests
+    {
+        private const string TESTFILESPATH = @"..\..\..\Testfiles\";
+        
+        [TestMethod]
+        public void FirstTest()
+        {
+            var generator = new RoslynParserGenerator();
+            GrammarTree grammarTree = LoadGrammar(TESTFILESPATH + @"simple expression1.tpg");
+            Grammar grammar = (Grammar) grammarTree.Eval();
+
+            string parserCsharpCode = generator.Generate(grammar, true);
+            
+            Assert.IsTrue(true);
+        }
+        
+        private GrammarTree LoadGrammar(string filename)
+        {
+            string grammarfile = System.IO.File.ReadAllText(filename);
+            Scanner scanner = new Scanner();
+            Parser parser = new Parser(scanner);
+            GrammarTree tree = (GrammarTree)parser.Parse(grammarfile, filename, new GrammarTree());
+            return tree;
+        }
+    }
+}
